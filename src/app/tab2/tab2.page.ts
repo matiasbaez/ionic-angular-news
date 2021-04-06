726351bf24a3f224b308dcaf3ffd24b347eb4ca3
Matias Báez <matiasbaez@users.noreply.github.com>
2021-04-06 17:21:46 -0400

    Updated, get news by category

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
+
+import { NoticiasService } from '../services/noticias.service';
+
+import { Article } from '../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +12,40 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  @ViewChild('categories_segment') segment: IonSegment;
+
+  categories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
+  noticias: Article[] = [];
+
+  constructor(
+    private noticiasService: NoticiasService,
+  ) {}
+
+  ngOnInit() { }
+
+  ngAfterViewInit() {
+    this.segment.value = this.categories[0];
+    this.getNewsByCategory(this.categories[0]);
+  }
+
+  segmentChanged(event) {
+    this.noticias = [];
+    this.getNewsByCategory(event.detail.value);
+  }
+
+  loadData(event) {
+    this.getNewsByCategory(this.segment.value, event);
+  }
+
+  getNewsByCategory(category, event?) {
+    this.noticiasService.getHeadlinesByCategory(category)
+    .subscribe(res => {
+      this.noticias.push(...res.articles);
+
+      if (event) {
+        event.target.complete();
+      }
+    });
+  }
 
 }
